ba7cbfba81b294acf514bbaa4cbb58f9d5f8a94d
shijun.dai <shijun_dai@163.com>
2020-07-29 18:13:49 +0800

    测试spring动态代理

--- src/main/java/cn/daisj/controller/HelloContorller.java
+++ src/main/java/cn/daisj/controller/HelloContorller.java
@@ -6,15 +6,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * 测试证明final类不能被spring aop代理
+ */
 @RestController
 @RequestMapping("/hello")
-public class HelloContorller {
+public /*final*/ class HelloContorller {
 
     @Autowired
     private PersonBean personBean;
 
+    /**
+     * final方法不能被aop
+     * @param param
+     * @return
+     */
     @GetMapping("/world")
-    public String hello(String param) {
+    public final String hello(String param) {
         return "Hello world!" + param;
     }
 
